478604a2c8f5114133b149c84855c94e6f9a5c7a
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-05-28 09:59:05 +0600

    test

---
+++ MicroServiceDemo/Catalog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.API.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        static string connectionString = new ConfigurationBuilder()
+            .SetBasePath(Directory
+            .GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build().GetConnectionString("Catalog.API");
+        static string databaseName = new ConfigurationBuilder()
+            .SetBasePath(Directory
+            .GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build().GetConnectionString("Catalog.API");
+        public CatalogDbContext(string connectionString, string databaseName) : base(connectionString, databaseName)
+        {
+        }
+    }
+}
