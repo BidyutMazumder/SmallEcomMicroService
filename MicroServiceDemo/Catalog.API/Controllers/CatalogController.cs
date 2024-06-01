b9d866669795b94ad9b24e88c9fceb7e0094f13a
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-06-01 18:11:50 +0600

    22 done

---
+++ MicroServiceDemo/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,38 @@
+﻿using Catalog.API.Interfaces.Manager;
+using Catalog.API.Models;
+using CoreApiResponse;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : BaseController
+    {
+        private readonly IProductManager _productManager;
+
+        public CatalogController(IProductManager productManager)
+        {
+            this._productManager = productManager;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public IActionResult GetProduct()
+        {
+            try
+            {
+                var product = _productManager.GetAll();
+                return CustomResult("ok", product);
+
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
