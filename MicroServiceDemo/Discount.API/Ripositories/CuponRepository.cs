850cecb0cec3499b9a46aef2a6dc15cee01fbaad
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-06-23 09:50:29 +0600

    ff

---
+++ MicroServiceDemo/Discount.API/Ripositories/CuponRepository.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Discount.API.Models;
+using Discount.API.Ripositories.Abstraction;
+using Npgsql;
+
+namespace Discount.API.Ripositories
+{
+    public class CuponRepository : ICuponRepository
+    {
+        IConfiguration _configuration;
+
+        public CuponRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(string productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Coupon> GetDiscount(string productId)
+        {
+            var Connection = new NpgsqlConnection(_configuration.GetConnectionString("DiscountDB"));
+            var Coupon = await Connection.QueryFirstOrDefaultAsync<Coupon>
+                ("select * from Coupon where ProductId = @ProductId", new { ProductId = productId });
+            if (Coupon == null)
+            {
+                return 
+            }
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
