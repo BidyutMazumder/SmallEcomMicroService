f3cb95b78a7b95d6c6a6e71ae35467e1a3aa21cc
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-07-10 12:54:01 +0600

    hh

--- MicroServiceDemo/Discount.Grpc/Services/DiscountService.cs
+++ MicroServiceDemo/Discount.Grpc/Services/DiscountService.cs
@@ -1,8 +1,18 @@
 ﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Ripositories.Abstraction;
 
 namespace Discount.Grpc.Services
 {
     public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
     {
+        private readonly ICuponRepository _cuponRepository;
+        private readonly ILogger<DiscountService> _logger;
+        public DiscountService(ICuponRepository cuponRepository, ILogger<DiscountService> logger) 
+        {
+            this._cuponRepository = cuponRepository;
+            this._logger = logger;
+        }
+        
+
     }
 }
