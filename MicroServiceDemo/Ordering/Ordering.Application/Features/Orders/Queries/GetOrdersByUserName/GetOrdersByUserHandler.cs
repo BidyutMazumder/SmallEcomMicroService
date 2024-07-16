8adc77ba9db94d17092a6aa36e5abca0efa88328
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-07-16 13:15:32 +0600

    77-2

---
+++ MicroServiceDemo/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersByUserName/GetOrdersByUserHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersByUserName
+{
+    public class GetOrdersByUserHandler: IRequestExceptionHandler<GetOrdersByUserQuery, List<OrderVm>>
+    {
+
+    }
+}
