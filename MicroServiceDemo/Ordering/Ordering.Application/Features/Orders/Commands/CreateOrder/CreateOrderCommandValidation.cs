bd4d7d6fccc1f6ec865e691a7a4ad37cc8503510
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-07-16 15:16:09 +0600

    79-1

--- MicroServiceDemo/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandValidation.cs
+++ MicroServiceDemo/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandValidation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Commands.CreateOrder
 {
-    internal class CreateOrderValidation
+    public class CreateOrderCommandValidation: AbstractValidator<CreateOrderCommand>
     {
+        public CreateOrderCommandValidation() 
+        { 
+            RuleFor(c => c.UserName).NotEmpty().WithMessage("Enter User Name")
+                .EmailAddress().WithMessage("Email should be your Username");
+            RuleFor(c => c.EmailAddress).NotEmpty().WithMessage("Enter Your Mail");
+            RuleFor(c => c.Amount).GreaterThan(0).WithMessage("non negative");
+            
+        }   
     }
 }
