0fb1a5128d531f9e1c2e5faf7e2cd0d4ccb09fbd
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-07-16 13:16:14 +0600

    77-3'

--- MicroServiceDemo/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersByUserName/GetOrdersByUserHandler.cs
+++ MicroServiceDemo/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersByUserName/GetOrdersByUserHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR.Pipeline;
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contacts.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,21 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersByUserName
 {
-    public class GetOrdersByUserHandler: IRequestExceptionHandler<GetOrdersByUserQuery, List<OrderVm>>
+    public class GetOrdersByUserHandler : IRequestHandler<GetOrdersByUserQuery, List<OrderVm>>
     {
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
 
+        public GetOrdersByUserHandler(IMapper mapper, IOrderRepository orderRepository)
+        {
+            this._mapper = mapper;
+            this._orderRepository = orderRepository;
+        }
+        public async Task<List<OrderVm>> Handle(GetOrdersByUserQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetOrdersByUserName(request.UserName);
+
+            return _mapper.Map<List<OrderVm>>(order);
+        }
     }
 }
