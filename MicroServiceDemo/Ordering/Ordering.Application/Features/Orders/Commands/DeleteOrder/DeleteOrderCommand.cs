b6c77ab13509c224f8c91881718b698bc4abc7d6
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-07-16 18:10:58 +0600

    kk

---
+++ MicroServiceDemo/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand: IRequest<bool>
+    {
+        public int Id { get; set; }
+        public DeleteOrderCommand(int id)
+        {
+            this.Id = id;
+        }
+    }
+}
