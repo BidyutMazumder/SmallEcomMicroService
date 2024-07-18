d386dfaf0bdf90b7e7b6183b09469085180b1ca3
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-07-18 08:19:53 +0600

    hh

---
+++ MicroServiceDemo/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Ordering.Application.Contacts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand, bool>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository)
+        {
+            this._orderRepository = orderRepository;
+        }
+        public async Task<bool> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            return await _orderRepository.DeleteAsync(new Domain.Models.Order() { Id = request.Id });
+        }
+    }
+}
