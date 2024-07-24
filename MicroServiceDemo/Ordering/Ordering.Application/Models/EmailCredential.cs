a816a1b24dd9c01d390b80c00bd7f7129cc158b2
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-07-24 14:57:58 +0600

    89

---
+++ MicroServiceDemo/Ordering/Ordering.Application/Models/EmailCredential.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Models
+{
+    public class EmailCredential
+    {
+        public static string EmailAddress = "bidyut.bdjobs@gmail.com";
+        public static string Password = "uqxb xijh fnrn uhsf";
+
+    }
+}
--- MicroServiceDemo/Ordering/Ordering.Infrastructer/Mail/EmailService.cs
+++ MicroServiceDemo/Ordering/Ordering.Infrastructer/Mail/EmailService.cs
@@ -1,12 +1,6 @@
 ﻿using Ordering.Application.Contacts.Infrastructure;
 using Ordering.Application.Models;
 using QuickMailer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Ordering.Infrastructer.Mail
 {
     public class EmailService : IEmailService
@@ -14,8 +8,11 @@ namespace Ordering.Infrastructer.Mail
         public async Task<bool>SendEmailAsync(EmailMessage emailMessage)
         {
             Email email =  new Email();
-            email.SendEmail(emailMessage.To, "", "", emailMessage.Subject, emailMessage.Body, []);
-            throw new NotImplementedException();
+            if (email.IsValidEmail(emailMessage.To))
+            {
+                return email.SendEmail(emailMessage.To, EmailCredential.EmailAddress, EmailCredential.Password, emailMessage.Subject, emailMessage.Body);
+            }
+            return false;
         }
     }
 }
