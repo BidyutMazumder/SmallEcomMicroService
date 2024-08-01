489c1aa67d19fac571930755c03111a224e50709
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-08-01 08:42:47 +0600

    100

---
+++ MicroServiceDemo/BuildingBlocks/EventBus.Messages/Events/BaseEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class BaseEvent
+    {
+        public BaseEvent()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreatedDate = DateTime.Now;
+        }
+        public Guid Id { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+    }
+}
---
+++ MicroServiceDemo/BuildingBlocks/EventBus.Messages/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class BasketCheckoutEvent: BaseEvent
+    {
+        public string UserName { get; set; }
+        public Decimal TotalPrice { get; set; }
+
+        //billing address
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        // payment
+
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string Expiraton { get; set; }
+        public int PaymentMethod { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
